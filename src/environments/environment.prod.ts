8485970ea7e55ae378e377c6c3863216dc0bd95d
Bakary DIALLO <41063166+dialbak@users.noreply.github.com>
2022-07-10 09:57:29 +0200

    mask api-key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyASCxHgCSpmVnQwVbLCHATcns5fJQFdmpY",
-    authDomain: "clips-36548.firebaseapp.com",
+    apiKey: "AIzaSyASCxHgCSpmVnQwVbLCHATcns5fJQF####",
+    authDomain: "clips-####.firebaseapp.com",
     projectId: "clips-36548",
     storageBucket: "clips-36548.appspot.com",
     appId: "1:798864185458:web:f93bd21de7f956bffc4aeb"
